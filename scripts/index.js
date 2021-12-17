39be10978461856f12def6efaf9b732e6886410d
WadimSh <wadim11@yandex.ru>
2021-12-17 13:31:02 +0300

    fix: внесены исправления в верстку и логику popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,46 @@
-const popupForm = document.querySelector('.popup');
-const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close-button');
-const inputForm = document.querySelector('.popup__form');
 const userName = document.querySelector('.profile__name');
 const userProf = document.querySelector('.profile__prof');
-const inputName = document.querySelector('#popup__name');
-const inputProf = document.querySelector('#popup__prof');
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button')
 
-function openPopup() {
-    popupForm.classList.add('popup_opened');
+const popupFormEdit = document.querySelector('.popup_type_profile');
+const closeButton = popupFormEdit.querySelector('.popup__close-button');
+const inputForm = popupFormEdit.querySelector('.popup__form');
+const inputName = popupFormEdit.querySelector('.popup__input_type_name');
+const inputProf = popupFormEdit.querySelector('.popup__input_type_prof');
+
+const popupFormAdd = document.querySelector('.popup_type_card');
+const closeButtonAdd = popupFormAdd.querySelector('.popup__close-button');
+
+
+function openPopupProfile() {
+    popupFormEdit.classList.add('popup_opened');
 	inputName.value = userName.textContent;
 	inputProf.value = userProf.textContent;
 }
 
-function closePopup() {
-    popupForm.classList.remove('popup_opened');
-	
+function closePopupProfile() {
+    popupFormEdit.classList.remove('popup_opened');
 }
 
-function formSubmitHandler (evt) {
+function formSubmitHandlerProfile(evt) {
 	evt.preventDefault(); 
 	userName.textContent = inputName.value;
 	userProf.textContent = inputProf.value;
-	closePopup();
+	closePopupProfile();
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-inputForm.addEventListener('submit', formSubmitHandler);
+function openPopupProfileAdd() {
+    popupFormAdd.classList.toggle('popup_opened');
+}
+
+function closePopupProfileAdd() {
+    popupFormAdd.classList.toggle('popup_opened');
+}
+
+
+editButton.addEventListener('click', openPopupProfile);
+closeButton.addEventListener('click', closePopupProfile);
+inputForm.addEventListener('submit', formSubmitHandlerProfile);
+addButton.addEventListener('click', openPopupProfileAdd);
+closeButtonAdd.addEventListener('click', closePopupProfileAdd);
